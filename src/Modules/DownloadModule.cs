4415394693ff95eb4f8b69532d6d460e3d382079
MadsHT <madsthyssen@live.dk>
2019-04-20 22:28:37 +0200

    Added a check for the amount of seeders and upped the file size

--- src/Modules/DownloadModule.cs
+++ src/Modules/DownloadModule.cs
@@ -15,156 +15,144 @@ using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 
-namespace Example.Modules
-{
-    [Name("Fun")]
-    public class DownloadModule : InteractiveBase
-    {
+namespace Example.Modules {
+    [Name ("Fun")]
+    public class DownloadModule : InteractiveBase {
         private readonly IConfigurationRoot _config;
-        private static HttpClient http = new HttpClient();
+        private static HttpClient http = new HttpClient ();
 
         private string path = "";
 
-        public DownloadModule(IConfigurationRoot config)
-        {
+        public DownloadModule (IConfigurationRoot config) {
             _config = config;
             path = _config["DBPath"];
         }
 
-
-        [Command("roll"), Alias("r")]
-        [Summary("roll x number of dice of y number of sides")]
-        [RequireUserPermission(ChannelPermission.SendMessages)]
-        public async Task Roll(int numDice, string die)
-        {
-            await ReplyAsync($"Rolling {numDice} times :game_die: ....");
+        [Command ("roll"), Alias ("r")]
+        [Summary ("roll x number of dice of y number of sides")]
+        [RequireUserPermission (ChannelPermission.SendMessages)]
+        public async Task Roll (int numDice, string die) {
+            await ReplyAsync ($"Rolling {numDice} times :game_die: ....");
 
             string msg = "";
-            await ReplyAsync(msg);
+            await ReplyAsync (msg);
         }
 
-        [Command("download"), Alias("dl")]
-        [Summary("I Will search for 1080p movies at reputable sources")]
-        [RequireUserPermission(GuildPermission.SendMessages)]
-        public async Task GetDownloadLinks(string imdbLink)
-        {
+        [Command ("download"), Alias ("dl")]
+        [Summary ("I Will search for 1080p movies at reputable sources")]
+        [RequireUserPermission (GuildPermission.SendMessages)]
+        public async Task GetDownloadLinks (string imdbLink) {
             //TODO make optional parameters
             var User = Context.User as SocketGuildUser;
-            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Plexer");
+            var role = Context.Guild.Roles.FirstOrDefault (x => x.Name == "Plexer");
 
-            if (!User.Roles.Contains(role))
-            {
-                await ReplyAsync(
+            if (!User.Roles.Contains (role)) {
+                await ReplyAsync (
                     "You do not have the required permission to use this command, please contact system admin");
                 return;
             }
-            
-            JToken toDo = SortList(imdbLink).Result;
-            
-            if (toDo != null)
-            {
-                await ReplyAsync($"Starting upload to serve");
-                using (var client = new WebClient())
-                {
-                    client.Credentials = new NetworkCredential(_config["ftpUser"], _config["ftpPass"]);
-                    client.UploadFile(
-                        "ftp://" + _config["serverName"] + "/Download/torrents/" + toDo["release_name"].ToString() +
+
+            JToken toDo = SortList (imdbLink).Result;
+
+            if (toDo != null) {
+                await ReplyAsync ($"Starting upload to serve");
+                using (var client = new WebClient ()) {
+                    //connnects to the ftp serve and uploads the file
+                    client.Credentials = new NetworkCredential (_config["ftpUser"], _config["ftpPass"]);
+                    client.UploadFile (
+                        "ftp://" + _config["serverName"] + "/Download/torrents/" + toDo["release_name"].ToString () +
                         ".torrent",
                         WebRequestMethods.Ftp.UploadFile,
-                        "./" + toDo["release_name"].ToString() + ".torrent");
+                        "./" + toDo["release_name"].ToString () + ".torrent");
                 }
 
-                System.IO.File.Delete("./" + toDo["release_name"].ToString() + ".torrent");
-                await ReplyAsync($"upload done.");
+                //Removes the file from bot storage
+                System.IO.File.Delete ("./" + toDo["release_name"].ToString () + ".torrent");
+                await ReplyAsync ($"upload done.");
             }
         }
 
-        public async Task<JToken> SortList(string imdbLink)
-        {
+        public async Task<JToken> SortList (string imdbLink) {
             JObject jo;
 
-            if (!imdbLink.Contains("https://www.imdb.com/title/"))
-            {
-                await ReplyAsync($"Given link is not from imdb");
+            //Check the link
+            if (imdbLink.Contains ("https://www.imdb.com/title/")) {
+                imdbLink = imdbLink.Split ("title/") [1].Split ("/") [0];
+            } else if (imdbLink.Contains ("tt")) {
+                
+            } else {
+                await ReplyAsync ($"Given link is not from imdb");
                 return null;
             }
 
-            imdbLink = imdbLink.Split("title/")[1].Split("/")[0];
+            await ReplyAsync ($"Looking for movie..");
 
-            await ReplyAsync($"Looking for movie..");
-
-            HttpResponseMessage response = await http.GetAsync(path + imdbLink);
-            if (response.IsSuccessStatusCode)
-            {
-                jo = JObject.Parse(await response.Content.ReadAsStringAsync());
+            //get the movie list from API
+            HttpResponseMessage response = await http.GetAsync (path + imdbLink);
+            if (response.IsSuccessStatusCode) {
+                jo = JObject.Parse (await response.Content.ReadAsStringAsync ());
 
                 JArray ja = null;
 
-                if (jo.First.First.HasValues)
-                {
-                    ja = JArray.FromObject(jo.First.First);
-                }
-                else
-                {
-                    await ReplyAsync("Nothing was found");
+                if (jo.First.First.HasValues) {
+                    ja = JArray.FromObject (jo.First.First);
+                } else {
+                    await ReplyAsync ("Nothing was found");
                     return null;
                 }
 
-                await ReplyAsync($"I found {ja.Count} plausibilities, sorting list");
+                await ReplyAsync ($"I found {ja.Count} plausibilities, sorting list");
 
-                List<JToken> tokenList = ja.ToList();
+                List<JToken> tokenList = ja.ToList ();
 
-                tokenList.RemoveAll(token => !token["release_name"].ToString().Contains("1080p")
-                                             || int.Parse(token["size"].ToString()) > 10000);
+                //Removes all non 1080p movies or are larger then 20GB or has no seeders
+                tokenList.RemoveAll (token => !token["release_name"].ToString ().Contains ("1080p") ||
+                    int.Parse (token["size"].ToString ()) > 20000 || int.Parse(token["seeders"].string) < 0);
 
-                if (tokenList.Count == 0)
-                {
-                    await ReplyAsync("Couldn't find suitable file");
+                if (tokenList.Count == 0) {
+                    await ReplyAsync ("Couldn't find suitable file");
                     return null;
                 }
 
-                tokenList = tokenList.OrderBy(size => size["seeders"]).ThenBy(seed => seed["size"]).ToList();
+                //Order the list by seeders then by size.
+                tokenList = tokenList.OrderBy (size => size["seeders"]).ThenBy (seed => seed["size"]).ToList ();
 
-                JToken theOne = tokenList.FirstOrDefault(token =>
-                    token["release_name"].ToString().ToLower().Contains("uni")
-                    || token["release_name"].ToString().ToLower().Contains("rapi"));
+                //Checks if there is any from unitail or rapidcows
+                JToken theOne = tokenList.FirstOrDefault (token =>
+                    token["release_name"].ToString ().ToLower ().Contains ("uni") ||
+                    token["release_name"].ToString ().ToLower ().Contains ("rapi"));
 
                 string msg = "";
 
-                if (tokenList.Count != 0 && theOne == null)
-                {
-                    await ReplyAsync("Found file. OBS ratio could become a problem");
-                    theOne = tokenList.FirstOrDefault();
-                } 
-                
-                if (theOne != null)
-                {
-                    
-                    msg += "Found the One: " + theOne["release_name"] 
-                                             + " Size: " + theOne["size"] 
-                                             + " Seeds: " + theOne["seeders"];
-                    
-                    await ReplyAsync($"```{msg}```");
-
-                    using (var client = new WebClient())
-                    {
-                        client.DownloadFile(theOne["download_url"].ToString(),
-                            "./" + theOne["release_name"].ToString() + ".torrent");
-                    }
-                    
-                    return theOne;
+                //Sets the one of reminds me about the ratio
+                if (tokenList.Count != 0 && theOne == null) {
+                    await ReplyAsync ("Found file. OBS ratio could become a problem");
+                    theOne = tokenList.FirstOrDefault ();
                 }
-                else
-                {
-                    await ReplyAsync("There was a problem with the download");
+
+                //Sends a msg with the info about the movie that it found
+                if (theOne != null) {
+                    msg += "Found the One: " + theOne["release_name"] +
+                        " Size: " + theOne["size"] +
+                        " Seeds: " + theOne["seeders"];
+
+                    await ReplyAsync ($"```{msg}```");
+
+                    //Downloads the file it found
+                    using (var client = new WebClient ()) {
+                        client.DownloadFile (theOne["download_url"].ToString (),
+                            "./" + theOne["release_name"].ToString () + ".torrent");
+                    }
+
+                    //Returns the one it found.
+                    return theOne;
+                } else {
+                    await ReplyAsync ("There was a problem with the download");
                     return null;
                 }
 
-                
-            }
-            else
-            {
-                await ReplyAsync("Couldn't find any movies with that ID");
+            } else {
+                await ReplyAsync ("Couldn't find any movies with that ID");
                 return null;
             }
         }
