adb3af430a462cfec285d69f57f11efe7740b790
MadsHT <madsthyssen@live.dk>
2019-07-12 12:03:17 +0200

    loging changes

--- src/Startup.cs
+++ src/Startup.cs
@@ -44,12 +44,12 @@ namespace Example
         {
             services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
             {                                       // Add discord to the collection
-                LogLevel = LogSeverity.Verbose,     // Tell the logger to give Verbose amount of info
+                LogLevel = LogSeverity.Error,     // Tell the logger to give Verbose amount of info
                 MessageCacheSize = 1000             // Cache 1,000 messages per channel
             }))
             .AddSingleton(new CommandService(new CommandServiceConfig
             {                                       // Add the command service to the collection
-                LogLevel = LogSeverity.Verbose,     // Tell the logger to give Verbose amount of info
+                LogLevel = LogSeverity.Error,     // Tell the logger to give Verbose amount of info
                 DefaultRunMode = RunMode.Async,     // Force all commands to run async by default
                 CaseSensitiveCommands = false       // Ignore case when executing commands
             }))
